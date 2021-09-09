f327e27211b38343a95e0de9b6b1542d90fce798
Jordan Alba <jordan008@live.ca>
2021-09-08 21:54:43 -0400

    Initial commit

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+
+@ExtendWith(SeleniumExtension.class)
+public class SeleniumTest {
+
+    EdgeDriver driver;
+
+    public SeleniumTest(EdgeDriver driver){
+        this.driver = driver;
+    }
+
+    @Test
+    public void test_fb_logo(){
+
+        //Arrange
+        //open web page
+        driver.get("https://www.facebook.com");
+        driver.manage().window().maximize();
+        //act
+        WebElement fbLogo = driver.findElement(By.className("fb_logo"));
+        //assert
+        assertThat(fbLogo.isDisplayed(), is(true));
+
+        try {
+            Thread.sleep(5000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        driver.quit();
+
+    }
+
+}
