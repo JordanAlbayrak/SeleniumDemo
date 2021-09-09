b438fefb6278e1c31afa5961e004de43b23b2104
Jordan Alba <jordan008@live.ca>
2021-09-08 22:37:48 -0400

    Set jdk back to 15.01 since changing it did not fix the issue. Also finished setup video. Test runs successfully.

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,9 +1,16 @@
+import com.codeborne.selenide.commands.TakeScreenshot;
 import io.github.bonigarcia.seljup.SeleniumExtension;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
@@ -11,14 +18,34 @@ import static org.hamcrest.junit.MatcherAssert.assertThat;
 @ExtendWith(SeleniumExtension.class)
 public class SeleniumTest {
 
-    EdgeDriver driver;
+    ChromeDriver driver;
+    private final String SCREENSHOTS = "./src/test/onDemandScreenshots";
 
-    public SeleniumTest(EdgeDriver driver){
+    public SeleniumTest(ChromeDriver driver){
         this.driver = driver;
+
+        DesiredCapabilities dc = new DesiredCapabilities();
+        dc.setCapability(CapabilityType.UNEXPECTED_ALERT_BEHAVIOUR, UnexpectedAlertBehaviour.IGNORE);
+        System.setProperty("sel.jup.screenshot.at.the.end.tests", "whenfailure");
+        System.setProperty("sel.jup.screenshot.format", "png");
+        System.setProperty("sel.jup.output.folder", "./src/test/failureScreenShots");
+    }
+
+    public static void takeSnapShot(WebDriver webDriver, String fileWithPath) throws Exception{
+
+        //Convert web driver object to TakeScreenshot
+        TakesScreenshot scrShot = ((TakesScreenshot)webDriver);
+        //call getScreenshotAs method to create the actual image file
+        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
+        //Move image file to new destination
+        File DestFile = new File(fileWithPath);
+        //Copy file at destination
+        FileUtils.copyFile(SrcFile, DestFile);
     }
 
     @Test
-    public void test_fb_logo(){
+    @DisplayName("test_facebook_logo")
+    void test_fb_logo(TestInfo testInfo) throws Exception{
 
         //Arrange
         //open web page
@@ -26,6 +53,10 @@ public class SeleniumTest {
         driver.manage().window().maximize();
         //act
         WebElement fbLogo = driver.findElement(By.className("fb_logo"));
+
+        String method = testInfo.getDisplayName();
+        takeSnapShot(driver, SCREENSHOTS + "\\" + method + "_" + System.currentTimeMillis() + ".png");
+
         //assert
         assertThat(fbLogo.isDisplayed(), is(true));
 
